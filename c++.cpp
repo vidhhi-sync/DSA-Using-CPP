a8a3d9e31ccd96f765978a9281151c573ecc2624
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 23:21:47 +0530

    Adding Introduction to C++

---
+++ c++.cpp
@@ -0,0 +1,25 @@
+//Generic Skeleton of C++
+/*#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hey Vidhi!"<<"\n"; //to print in next use \n
+    //instead of a \n we can use endl
+    cout<<"Hey Vidhi!"<<endl;
+    cout<<"Hey Vidhi!";
+    return 0;
+}
+
+//taking integer as an input
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter value of x:";
+      cout<<"Enter value of y:";
+    cin>>x>>y;
+    cout<<"Value of x is:"<< x << "And y is :"<<y;
+    return 0;
+}*/
+
+//Including all the libraries in one go, we use:
+#include<bits/stdc++.h>

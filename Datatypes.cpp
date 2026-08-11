1f73946e80b498fae31937eb94e7fa1509b2d316
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 23:53:59 +0530

    Adding Datatypes

---
+++ Datatypes.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //int and long datatype
+     int x=10;
+     long y = 100;
+     long long z = 100000000;
+     cout<<x<<endl<<y<<endl<<z<<endl;
+     //float and double datatype
+     float a = 2.344;
+     double b = 10;
+     cout<<a<<endl<<b;
+     return 0;
+}
